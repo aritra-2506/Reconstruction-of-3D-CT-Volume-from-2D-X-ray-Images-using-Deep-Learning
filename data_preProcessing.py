64b22d66bd7bf2568ff3e08611281747148947b0
Aritra Mazumdar <35579668+aritra-2506@users.noreply.github.com>
2020-04-10 14:28:47 +0200

    Update data_preProcessing.py

--- data_preProcessing.py
+++ data_preProcessing.py
@@ -7,6 +7,7 @@ from vtkplotter import Volume
 import sys, os
 import matplotlib.pyplot as plt
 
+
 def main(folderPath):
     st = time.time()
     my_glob = glob.glob1(folderPath, "*")
@@ -49,9 +50,9 @@ def main(folderPath):
                 cs = [float(q) for q in tem[j].ImageOrientationPatient]
                 ipp = [float(q) for q in tem[j].ImagePositionPatient]
                 parity = pow(-1, j)
-                dist += parity*(cs[1]*cs[5] - cs[2]*cs[4])*ipp[0]
-                dist += parity*(cs[2]*cs[3] - cs[0]*cs[5])*ipp[1]
-                dist += parity*(cs[0]*cs[4] - cs[1]*cs[3])*ipp[2]
+                dist += parity * (cs[1] * cs[5] - cs[2] * cs[4]) * ipp[0]
+                dist += parity * (cs[2] * cs[3] - cs[0] * cs[5]) * ipp[1]
+                dist += parity * (cs[0] * cs[4] - cs[1] * cs[3]) * ipp[2]
             z_spacing = abs(dist)
             slope = tem[i].RescaleSlope
             intercept = tem[i].RescaleIntercept
@@ -59,54 +60,54 @@ def main(folderPath):
         scaledIm[:, :, i] = arr
 
     # convert to hounsfield units
-    scaledIm = slope*scaledIm + intercept
+    scaledIm = slope * scaledIm + intercept
     pix_spacing.append(z_spacing)
 
-    wl = 300    # window parameters for Angio
+    wl = 300  # window parameters for Angio
     ww = 600
 
     windowed = np.zeros(imShape, dtype=np.uint8)
     # allImages[scaledIm <= (wl-0.5-(ww-1)/2.0)] = 0
-    k = np.logical_and(scaledIm > (wl-0.5-(ww-1)/2.0), scaledIm <= (wl-0.5+(ww-1)/2.0))
-    windowed[k] = ((scaledIm[k] - (wl-0.5))/(ww-1)+0.5)*255
-    windowed[scaledIm > (wl-0.5+(ww-1)/2.0)] = 255
-    
-    #Rotations
-    windowed=np.rot90(windowed)
+    k = np.logical_and(scaledIm > (wl - 0.5 - (ww - 1) / 2.0), scaledIm <= (wl - 0.5 + (ww - 1) / 2.0))
+    windowed[k] = ((scaledIm[k] - (wl - 0.5)) / (ww - 1) + 0.5) * 255
+    windowed[scaledIm > (wl - 0.5 + (ww - 1) / 2.0)] = 255
+
+    # Rotations
     windowed = np.rot90(windowed)
     windowed = np.rot90(windowed)
-    
-    #Data Slicing for Ribs, Vascular and Spine
-    windowed=np.transpose(windowed)
-    windowed_R=windowed[0:50]
+    windowed = np.rot90(windowed)
+
+    # Data Slicing for Ribs, Vascular and Spine
+    windowed = np.transpose(windowed)
+    windowed_R = windowed[0:50]
     windowed_V = windowed[50:100]
     windowed_S = windowed[100:150]
     windowed_R = np.transpose(windowed_R)
     windowed_V = np.transpose(windowed_V)
     windowed_S = np.transpose(windowed_S)
     windowed = np.transpose(windowed)
-    
-    #Zero Padding Sliced Data for Original Volume
-    m=np.zeros((512, 512))
-    j=m
-    l=m
-    for x in range(0,98):
+
+    # Zero Padding Sliced Data for Original Volume
+    m = np.zeros((512, 512))
+    j = m
+    l = m
+    for x in range(0, 98):
         j = np.dstack((j, m))
-    for y in range(0,48):
+    for y in range(0, 48):
         l = np.dstack((l, m))
-    windowed_R=np.dstack((windowed_R, j))
+    windowed_R = np.dstack((windowed_R, j))
     k = np.dstack((j, m))
-    windowed_S = np.dstack((k,windowed_S))
-    o=l
+    windowed_S = np.dstack((k, windowed_S))
+    o = l
     o = np.dstack((o, m))
     windowed_V = np.dstack((o, windowed_V))
-    windowed_V = np.dstack((windowed_V,l))
-    
-    #3D DICOM Data
-    a=windowed
-    
-    #Creating 2D DRR Label Data for Original and Sliced Volumes
-    b=windowed.sum(2)
+    windowed_V = np.dstack((windowed_V, l))
+
+    # 3D DICOM Data
+    a = windowed
+
+    # Creating 2D DRR Label Data for Original and Sliced Volumes
+    b = windowed.sum(2)
     c = windowed_R.sum(2)
     d = windowed_V.sum(2)
     e = windowed_S.sum(2)
@@ -115,26 +116,29 @@ def main(folderPath):
 
 if __name__ == '__main__':
     folder = "C:/Users/Aritra Mazumdar/Downloads/ISIC/3000568-87015/"
-    p,q,r,s,t,u=main(folder)
-    
-    #3D DICOM Data Visualization
+    p, q, r, s, t, u = main(folder)
+
+    # 3D DICOM Data Visualization
     vol = Volume(p, spacing=u)
     vol.permuteAxes(2, 1, 0).mirror("y")
     vol.show(bg="black")
-    
-    #2D DRR Plots
+
+    # 2D DRR Plots
     fig = plt.figure()
-    #Complete DRR
+    # Complete DRR
     a1 = plt.subplot(2, 2, 1)
+    a1.title.set_text('DRR Whole')
     a1.imshow(q, cmap=plt.cm.bone, aspect=1)
-    #DRR Ribs
+    # DRR Ribs
     a2 = plt.subplot(2, 2, 2)
+    a2.title.set_text('DRR Ribs')
     a2.imshow(r, cmap=plt.cm.bone, aspect=1)
-    #DRR Vascular
+    # DRR Vascular
     a3 = plt.subplot(2, 2, 3)
+    a3.title.set_text('DRR Vascular')
     a3.imshow(s, cmap=plt.cm.bone, aspect=1)
-    #DRR Spine
+    # DRR Spine
     a4 = plt.subplot(2, 2, 4)
+    a4.title.set_text('DRR Spine')
     a4.imshow(t, cmap=plt.cm.bone, aspect=1)
     plt.show()
-
