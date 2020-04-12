7e1947f0f6840cbe5fb4de8c6bd926623e6573e1
Aritra Mazumdar <35579668+aritra-2506@users.noreply.github.com>
2020-04-12 19:43:42 +0200

    Update data_preProcessing.py

--- data_preProcessing.py
+++ data_preProcessing.py
@@ -2,10 +2,10 @@ import time
 import glob
 import pydicom
 import numpy as np
-import vtkplotter
 from vtkplotter import Volume
-import sys, os
+import os
 import matplotlib.pyplot as plt
+import cv2
 
 
 def main(folderPath):
@@ -72,51 +72,53 @@ def main(folderPath):
     windowed[k] = ((scaledIm[k] - (wl - 0.5)) / (ww - 1) + 0.5) * 255
     windowed[scaledIm > (wl - 0.5 + (ww - 1) / 2.0)] = 255
 
-    # Rotations
-    windowed = np.rot90(windowed)
-    windowed = np.rot90(windowed)
-    windowed = np.rot90(windowed)
-
     # Data Slicing for Ribs, Vascular and Spine
-    windowed = np.transpose(windowed)
-    windowed_R = windowed[0:50]
-    windowed_V = windowed[50:100]
-    windowed_S = windowed[100:150]
+    windowed_R = windowed[0:180]
+    windowed_V = windowed[180:360]
+    windowed_S = windowed[360:512]
+
     windowed_R = np.transpose(windowed_R)
     windowed_V = np.transpose(windowed_V)
     windowed_S = np.transpose(windowed_S)
     windowed = np.transpose(windowed)
 
     # Zero Padding Sliced Data for Original Volume
-    m = np.zeros((512, 512))
+    m = np.zeros((149, 512))
     j = m
     l = m
-    for x in range(0, 98):
+    p=m
+    q=m
+    for x in range(0, 331):
         j = np.dstack((j, m))
-    for y in range(0, 48):
+    for x in range(0, 359):
+        q = np.dstack((q, m))
+    for y in range(0, 179):
         l = np.dstack((l, m))
+    for y in range(0, 151):
+        p = np.dstack((p, m))
     windowed_R = np.dstack((windowed_R, j))
-    k = np.dstack((j, m))
-    windowed_S = np.dstack((k, windowed_S))
-    o = l
-    o = np.dstack((o, m))
-    windowed_V = np.dstack((o, windowed_V))
-    windowed_V = np.dstack((windowed_V, l))
+    windowed_S = np.dstack((q, windowed_S))
+    windowed_V = np.dstack((l, windowed_V))
+    windowed_V = np.dstack((windowed_V, p))
 
     # 3D DICOM Data
-    a = windowed
+    a = np.transpose(windowed)
 
     # Creating 2D DRR Label Data for Original and Sliced Volumes
     b = windowed.sum(2)
     c = windowed_R.sum(2)
     d = windowed_V.sum(2)
     e = windowed_S.sum(2)
-    return a, b, c, d, e, pix_spacing
 
+    return a, b, c, d, e, pix_spacing
 
 if __name__ == '__main__':
     folder = "C:/Users/Aritra Mazumdar/Downloads/ISIC/3000568-87015/"
     p, q, r, s, t, u = main(folder)
+    labels_D0 = []
+    labels_D1 = []
+    labels_D2 = []
+    labels_D3 = []
 
     # 3D DICOM Data Visualization
     vol = Volume(p, spacing=u)
@@ -124,21 +126,54 @@ if __name__ == '__main__':
     vol.show(bg="black")
 
     # 2D DRR Plots
-    fig = plt.figure()
+    fig1 = plt.figure()
     # Complete DRR
-    a1 = plt.subplot(2, 2, 1)
-    a1.title.set_text('DRR Whole')
-    a1.imshow(q, cmap=plt.cm.bone, aspect=1)
+    a1 = fig1.add_subplot(2, 2, 1)
+    a1.set_title('DRR Whole')
+    a1.imshow(q, cmap=plt.cm.bone, aspect=3)
     # DRR Ribs
-    a2 = plt.subplot(2, 2, 2)
-    a2.title.set_text('DRR Ribs')
-    a2.imshow(r, cmap=plt.cm.bone, aspect=1)
+    a2 = fig1.add_subplot(2, 2, 2)
+    a2.set_title('DRR Ribs')
+    a2.imshow(r, cmap=plt.cm.bone, aspect=3)
     # DRR Vascular
-    a3 = plt.subplot(2, 2, 3)
-    a3.title.set_text('DRR Vascular')
-    a3.imshow(s, cmap=plt.cm.bone, aspect=1)
+    a3 = fig1.add_subplot(2, 2, 3)
+    a3.set_title('DRR Vascular')
+    a3.imshow(s, cmap=plt.cm.bone, aspect=3)
     # DRR Spine
-    a4 = plt.subplot(2, 2, 4)
-    a4.title.set_text('DRR Spine')
-    a4.imshow(t, cmap=plt.cm.bone, aspect=1)
+    a4 = fig1.add_subplot(2, 2, 4)
+    a4.set_title('DRR Spine')
+    a4.imshow(t, cmap=plt.cm.bone, aspect=3)
+
+    #Generating Labels
+    labels_D0.append(q)
+    labels_D0 = np.asarray(labels_D0)
+
+    labels_D1.append(r)
+    labels_D1 = np.asarray(labels_D1)
+
+    labels_D2.append(s)
+    labels_D2 = np.asarray(labels_D2)
+
+    labels_D3.append(t)
+    labels_D3 = np.asarray(labels_D3)
+
+    #Plotting DRR Labels
+    fig2 = plt.figure()
+    # DRR Ribs
+    a11 = fig2.add_subplot(2, 2, 1)
+    a11.set_title('DRR Whole')
+    a11.imshow(labels_D0[0], cmap=plt.cm.bone, aspect=3)
+    #DRR Ribs
+    a22 = fig2.add_subplot(2, 2, 2)
+    a22.set_title('DRR Ribs')
+    a22.imshow(labels_D1[0], cmap=plt.cm.bone, aspect=3)
+    # DRR Vascular
+    a33 = fig2.add_subplot(2, 2, 3)
+    a33.set_title('DRR Vascular')
+    a33.imshow(labels_D2[0], cmap=plt.cm.bone, aspect=3)
+    # DRR Spine
+    a44 = fig2.add_subplot(2, 2, 4)
+    a44.set_title('DRR Spine')
+    a44.imshow(labels_D3[0], cmap=plt.cm.bone, aspect=3)
+
     plt.show()
