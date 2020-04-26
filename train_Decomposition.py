29b5c1e951d92da0639424bfd2c052e66a75adda
Aritra Mazumdar <35579668+aritra-2506@users.noreply.github.com>
2020-04-26 15:21:47 +0200

    Update train_Decomposition.py

--- train_Decomposition.py
+++ train_Decomposition.py
@@ -1,7 +1,7 @@
 import cv2
 import glob
 import numpy as np
-from keras.layers import Conv2D, MaxPooling2D, Dropout, Conv2DTranspose, concatenate, Input
+from keras.layers import Conv2D, MaxPooling2D, Dropout, Conv2DTranspose, concatenate, Input, Lambda, Reshape
 import keras.backend as k
 from keras.models import Model
 import os
@@ -9,66 +9,107 @@ import matplotlib.pyplot as plt
 import pydicom
 import keras.layers as h
 
-PathDicom = "C:/Users/Aritra Mazumdar/Downloads/ISIC/3000923-62357/"
+PathDicom_W = "C:/Users/Aritra Mazumdar/Downloads/ISIC/Dataset/Label_W"
+PathDicom_R = "C:/Users/Aritra Mazumdar/Downloads/ISIC/Dataset/Label_R"
+PathDicom_S = "C:/Users/Aritra Mazumdar/Downloads/ISIC/Dataset/Label_S"
+PathDicom_V = "C:/Users/Aritra Mazumdar/Downloads/ISIC/Dataset/Label_V"
 
-lstFilesDCM = []  # create an empty list
-for dirName, subdirList, fileList in os.walk(PathDicom):
+lstFilesDCM_W = []  # create an empty list
+for dirName, subdirList, fileList in os.walk(PathDicom_W):
     for filename in fileList:
         if ".dcm" in filename.lower():  # check whether the file's DICOM
-            lstFilesDCM.append(os.path.join(dirName,filename))
+            lstFilesDCM_W.append(os.path.join(dirName,filename))
 
+lstFilesDCM_R = []  # create an empty list
+for dirName, subdirList, fileList in os.walk(PathDicom_R):
+    for filename in fileList:
+        if ".dcm" in filename.lower():  # check whether the file's DICOM
+            lstFilesDCM_R.append(os.path.join(dirName,filename))
+
+lstFilesDCM_V = []  # create an empty list
+for dirName, subdirList, fileList in os.walk(PathDicom_V):
+    for filename in fileList:
+        if ".dcm" in filename.lower():  # check whether the file's DICOM
+            lstFilesDCM_V.append(os.path.join(dirName, filename))
+
+lstFilesDCM_S = []  # create an empty list
+for dirName, subdirList, fileList in os.walk(PathDicom_S):
+    for filename in fileList:
+        if ".dcm" in filename.lower():  # check whether the file's DICOM
+            lstFilesDCM_S.append(os.path.join(dirName,filename))
 
 images=[]
 labels_D1=[]
 labels_D2=[]
 labels_D3=[]
 
-for filenameDCM in lstFilesDCM:
+for filenameDCM in lstFilesDCM_W:
     # read the file
     ds = pydicom.read_file(filenameDCM)
     x=cv2.resize(ds.pixel_array, dsize=(256, 256), interpolation=cv2.INTER_CUBIC)
     # store the raw image data
     images.append(x)
-    labels_D1.append(x)
-    labels_D2.append(x)
-    labels_D3.append(x)
 
+for filenameDCM in lstFilesDCM_R:
+    # read the file
+    ds = pydicom.read_file(filenameDCM)
+    x=cv2.resize(ds.pixel_array, dsize=(256, 256), interpolation=cv2.INTER_CUBIC)
+    # store the raw image data
+    labels_D1.append(x)
+
+for filenameDCM in lstFilesDCM_V:
+    # read the file
+    ds = pydicom.read_file(filenameDCM)
+    x=cv2.resize(ds.pixel_array, dsize=(256, 256), interpolation=cv2.INTER_CUBIC)
+    # store the raw image data
+    labels_D2.append(x)
+
+for filenameDCM in lstFilesDCM_S:
+    # read the file
+    ds = pydicom.read_file(filenameDCM)
+    x=cv2.resize(ds.pixel_array, dsize=(256, 256), interpolation=cv2.INTER_CUBIC)
+    # store the raw image data
+    labels_D3.append(x)
 
 images=np.asarray(images)
 images = cv2.normalize(images, None, alpha = 0, beta = 255, norm_type = cv2.NORM_MINMAX, dtype = cv2.CV_32F)
 images.astype(np.uint8)
-images = images.reshape((2, 256, 256, 1))
+images = images.reshape((4, 256, 256, 1))
 images = images.astype('float32') / 255
-#images=np.transpose(images)
 
 labels_R=images
 
 labels_D1=np.asarray(labels_D1)
 labels_D1 = cv2.normalize(labels_D1, None, alpha = 0, beta = 255, norm_type = cv2.NORM_MINMAX, dtype = cv2.CV_32F)
 labels_D1.astype(np.uint8)
-labels_D1 = labels_D1.reshape((2, 256, 256, 1))
+labels_D1 = labels_D1.reshape((4, 256, 256, 1))
 labels_D1 = labels_D1.astype('float32') / 255
-#labels_D1=np.transpose(labels_D1)
 
 labels_D2=np.asarray(labels_D2)
 labels_D2 = cv2.normalize(labels_D2, None, alpha = 0, beta = 255, norm_type = cv2.NORM_MINMAX, dtype = cv2.CV_32F)
 labels_D2.astype(np.uint8)
-labels_D2 = labels_D2.reshape((2, 256, 256, 1))
+labels_D2 = labels_D2.reshape((4, 256, 256, 1))
 labels_D2 = labels_D2.astype('float32') / 255
-#labels_D2=np.transpose(labels_D2)
 
 labels_D3=np.asarray(labels_D3)
 labels_D3 = cv2.normalize(labels_D3, None, alpha = 0, beta = 255, norm_type = cv2.NORM_MINMAX, dtype = cv2.CV_32F)
 labels_D3.astype(np.uint8)
-labels_D3 = labels_D3.reshape((2, 256, 256, 1))
+labels_D3 = labels_D3.reshape((4, 256, 256, 1))
 labels_D3 = labels_D3.astype('float32') / 255
-#labels_D3=np.transpose(labels_D3)
 
 
+''''#Train-Test split
+images_train=images[0:3]
+images_test=images[3:5]
+labels_D1_train=labels_D1[0:3]
+labels_D1_test=labels_D1[3:5]
+labels_D2_train=labels_D2[0:3]
+labels_D2_test=labels_D1[3:5]
+labels_D3_train=labels_D3[0:3]
+labels_D3_test=labels_D3[3:5]
+labels_R_train=labels_R[0:3]
+labels_R_test=labels_D1[3:5]'''
 
-'''#Train-Test split
-images_train=images[0:8]
-images_test=images[8:10]'''
 
 #Buliding Network
 def build_model(input_img):
@@ -108,10 +149,14 @@ def build_model(input_img):
     uconv2 = Conv2D(192, (3, 3), activation="relu", padding="same")(uconv2)
     uconv2 = Conv2D(64, (3, 3), activation="relu", padding="same")(uconv2)
 
-    output_img1 = Conv2D(1, (1, 1), activation='sigmoid', padding='same')(uconv2)
-    output_img2 = Conv2D(1, (1, 1), activation='sigmoid', padding='same')(uconv2)
-    output_img3 = Conv2D(1, (1, 1), activation='sigmoid', padding='same')(uconv2)
+    output_img = Conv2D(3, (1, 1), activation='sigmoid', padding='same')(uconv2)
+    output_img1 = Lambda(lambda output_img: output_img[:, :, :, 0])(output_img)
+    output_img2 = Lambda(lambda output_img: output_img[:, :, :, 0])(output_img)
+    output_img3 = Lambda(lambda output_img: output_img[:, :, :, 0])(output_img)
 
+    output_img1 = Reshape((256, 256, 1))(output_img1)
+    output_img2 = Reshape((256, 256, 1))(output_img2)
+    output_img3 = Reshape((256, 256, 1))(output_img3)
 
     return output_img1, output_img2, output_img3
 
@@ -119,20 +164,78 @@ input_img = Input(shape=(256,256,1))
 labels_layer_D1=Input(shape=(256,256,1))
 labels_layer_D2=Input(shape=(256,256,1))
 labels_layer_D3=Input(shape=(256,256,1))
-labels_layer_R=Input(shape=(256,256,1))
 output_img1, output_img2, output_img3=build_model(input_img)
 
 
 model = Model(inputs=[input_img, labels_layer_D1, labels_layer_D2, labels_layer_D3], outputs=[output_img1, output_img2, output_img3])
 
-l1=0.1*(k.sqrt(k.sum(k.square(h.Subtract()([output_img1,labels_layer_D1])))))+0.9*(k.sum(k.abs(h.Subtract()([output_img1,labels_layer_D1]))))
-l2=0.1*(k.sqrt(k.sum(k.square(h.Subtract()([output_img2,labels_layer_D2])))))+0.9*(k.sum(k.abs(h.Subtract()([output_img2,labels_layer_D2]))))
-l3=0.1*(k.sqrt(k.sum(k.square(h.Subtract()([output_img3,labels_layer_D3])))))+0.9*(k.sum(k.abs(h.Subtract()([output_img3,labels_layer_D3]))))
-loss=l1+l2+l3
+batch_size=4
+
+#styles - style 3 and 4 best
+#style1
+'''l1_D=0.1*(k.sqrt(k.sum(k.square(h.Subtract()([output_img1,labels_layer_D1])))))+0.9*(k.sum(k.abs(h.Subtract()([output_img1,labels_layer_D1]))))
+l2_D=0.1*(k.sqrt(k.sum(k.square(h.Subtract()([output_img2,labels_layer_D2])))))+0.9*(k.sum(k.abs(h.Subtract()([output_img2,labels_layer_D2]))))
+l3_D=0.1*(k.sqrt(k.sum(k.square(h.Subtract()([output_img3,labels_layer_D3])))))+0.9*(k.sum(k.abs(h.Subtract()([output_img3,labels_layer_D3]))))
+loss=(l1_d+l2_D+l3_D)/batch_size'''
+
+#style2
+'''l1_D2=(k.abs(output_img1-labels_layer_D1))
+l2_D2=(k.abs(output_img2-labels_layer_D2))
+l3_D2=(k.abs(output_img3-labels_layer_D3))
+l_D2=0.9*(l1_D2+l2_D2+l3_D2)
+
+l1_D1=k.square(output_img1-labels_layer_D1)
+l2_D1=k.square(output_img2-labels_layer_D2)
+l3_D1=k.square(output_img3-labels_layer_D3)
+l_D1=0.1*k.sqrt(l1_D1+l2_D1+l3_D1)
+
+loss=(l_D1+l_D2)/batch_size'''
+
+#style3
+loss1=0
+loss2=0
+
+for i in range (0, batch_size):
+    l1=k.abs(output_img1[i]-labels_layer_D1[i])
+    l2=k.abs(output_img2[i] - labels_layer_D2[i])
+    l3=k.abs(output_img3[i] - labels_layer_D3[i])
+    l=l1+l2+l3
+    loss1=loss1+l
+    loss2 = loss2+k.square(l)
+
+loss1D=0.9*loss1/batch_size
+loss2D=0.1*k.sqrt(loss2)/batch_size
+loss=loss1D+loss2D
+
+'''#style4
+loss2=0
+
+for i in range (0, batch_size):
+    l1=(output_img1[i]-labels_layer_D1[i])
+    l2=(output_img2[i] - labels_layer_D2[i])
+    l3=(output_img3[i] - labels_layer_D3[i])
+    l=l1+l2+l3
+    loss1=0.9*k.sum(k.abs(l))+0.1*k.sqrt(k.sum(k.square(l)))
+    loss2 = loss2+loss1
+
+loss=loss2/batch_size'''
+
+max_pixel = 1.0
+metric=(10.0 * k.log((max_pixel ** 2) / (k.mean(k.square(output_img - labels_layer_R), axis=-1)))) / 2.303
+
 model.add_loss(loss)
+model.add_metric(metric)
 
 #Compiling
 model.compile(optimizer='adam')
 
 #Fitting
-model.fit([images, labels_D1, labels_D2, labels_D3], epochs=2)
+model.fit([images, labels_D1, labels_D2, labels_D3], epochs=10, batch_size=batch_size, shuffle=True)
+
+# serialize model to JSON
+model_json = model.to_json()
+with open("model_new.json", "w") as json_file:
+    json_file.write(model_json)
+# serialize weights to HDF5
+model.save_weights("model_new.h5")
+print("Saved model to disk")
