43b890c729afafd95433ca28ad6277a89c78011e
Aritra Mazumdar <35579668+aritra-2506@users.noreply.github.com>
2020-04-27 13:03:59 +0200

    Add files via upload

---
+++ preproc.py
@@ -0,0 +1,28 @@
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+import pydicom
+import cv2
+
+PathDicom = "C:/Users/Aritra Mazumdar/Downloads/ISIC/3000566-03192"
+#PathDicom = "C:/Users/Aritra Mazumdar/Downloads/ISIC/New/Spine"
+#PathDicom = "C:/Users/Aritra Mazumdar/Downloads/ISIC/New/Vascular"
+
+#lstFilesDCM = []  # create an empty list
+for dirName, subdirList, fileList in os.walk(PathDicom):
+    for filename in fileList:
+        if ".dcm" in filename.lower():  # check whether the file's DICOM
+
+            ds = pydicom.read_file(os.path.join(dirName, filename))
+            j = ds.pixel_array
+            #Ribs
+            #j[162:513].fill(0)
+            #Spine
+            #j[0:327].fill(0)
+            #Vascular
+            j[0:162].fill(0)
+            j[327:513].fill(0)
+            ds.PixelData = j.tostring()
+            ds.save_as(os.path.join(dirName, filename))
+
+
