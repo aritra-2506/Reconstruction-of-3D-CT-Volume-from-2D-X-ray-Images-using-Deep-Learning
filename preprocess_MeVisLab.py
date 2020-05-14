ce1bd09d938a792ee16049b193421ba364522a9b
Aritra Mazumdar <35579668+aritra-2506@users.noreply.github.com>
2020-05-14 17:01:04 +0200

    Update preprocess_MeVisLab.py

--- preprocess_MeVisLab.py
+++ preprocess_MeVisLab.py
@@ -4,7 +4,9 @@ import matplotlib.pyplot as plt
 import pydicom
 import cv2
 
-PathDicom = "C:/Users/Aritra Mazumdar/Downloads/ISIC/3000566-03192"
+PathDicom = "C:/Users/Aritra Mazumdar/Downloads/ISIC/3000568-87015"
+#PathDicom = "C:/Users/Aritra Mazumdar/Downloads/ISIC/New/Spine"
+#PathDicom = "C:/Users/Aritra Mazumdar/Downloads/ISIC/New/Vascular"
 
 #lstFilesDCM = []  # create an empty list
 for dirName, subdirList, fileList in os.walk(PathDicom):
@@ -12,14 +14,20 @@ for dirName, subdirList, fileList in os.walk(PathDicom):
         if ".dcm" in filename.lower():  # check whether the file's DICOM
 
             ds = pydicom.read_file(os.path.join(dirName, filename))
+
             j = ds.pixel_array
+            # Whole
+            #j = np.rot90(j)
             #Ribs
-            #j[162:513].fill(0)
+            j[162:513].fill(0)
+            j = np.rot90(j)
             #Spine
             #j[0:327].fill(0)
+            #j = np.rot90(j)
             #Vascular
             j[0:162].fill(0)
             j[327:513].fill(0)
+            #j = np.rot90(j)
             ds.PixelData = j.tostring()
             ds.save_as(os.path.join(dirName, filename))
 
