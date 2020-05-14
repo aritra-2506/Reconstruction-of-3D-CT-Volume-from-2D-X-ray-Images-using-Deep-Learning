6557dd6d8dafa8d3f9ea692a2a7d6fd187626978
Aritra Mazumdar <35579668+aritra-2506@users.noreply.github.com>
2020-05-14 17:01:26 +0200

    Update preprocess_MeVisLab.py

--- preprocess_MeVisLab.py
+++ preprocess_MeVisLab.py
@@ -5,8 +5,6 @@ import pydicom
 import cv2
 
 PathDicom = "C:/Users/Aritra Mazumdar/Downloads/ISIC/3000568-87015"
-#PathDicom = "C:/Users/Aritra Mazumdar/Downloads/ISIC/New/Spine"
-#PathDicom = "C:/Users/Aritra Mazumdar/Downloads/ISIC/New/Vascular"
 
 #lstFilesDCM = []  # create an empty list
 for dirName, subdirList, fileList in os.walk(PathDicom):
