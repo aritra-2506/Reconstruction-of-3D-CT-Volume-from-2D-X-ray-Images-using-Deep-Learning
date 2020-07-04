5de2b2fb9bcc6fe62c905cc92a98690bb7e71feb
Aritra Mazumdar <35579668+aritra-2506@users.noreply.github.com>
2020-07-04 14:25:34 +0200

    Update drr2ct_Decomposition_Reconstruction_PyTorch.py

--- drr2ct_Decomposition_Reconstruction_PyTorch.py
+++ drr2ct_Decomposition_Reconstruction_PyTorch.py
@@ -35,7 +35,7 @@ PathDicom_images = "/content/drive/My Drive/NewDataset/images"
 PathDicom_labels = "/content/drive/My Drive/NewDataset/labels"
 
 
-dss=pydicom.read_file("/content/drive/My Drive/NewDataset/resave/1.dcm")
+#dss=pydicom.read_file("/content/drive/My Drive/NewDataset/resave/1.dcm")
 
 images=[]
 for dirName, subdirList, fileList in os.walk(PathDicom_images):
@@ -499,46 +499,157 @@ g.show()
 
 
 #app
-def write_dicom(image2d, i, slicesLocation, dss):
+def write_dicom(image2d, i, slicesLocation, filename_little_endian, dt, siuid):
+    meta = FileMetaDataset()
+    meta.MediaStorageSOPClassUID = pydicom._storage_sopclass_uids.CTImageStorage
+    meta.MediaStorageSOPInstanceUID = pydicom.uid.generate_uid()
+    meta.TransferSyntaxUID = pydicom.uid.ExplicitVRLittleEndian
+    meta.FileMetaInformationGroupLength = 150
+    meta.ImplementationClassUID = "1.3.6.1.4.1.22213.1.143"
+
+    meta.ImplementationVersionName = '0.5'
+    meta.SourceApplicationEntityTitle = 'POSDA'
 
 
-    ds=dss
-    ds.InstanceNumber = i + 1
+    ds = FileDataset(filename_little_endian, {},
+                     file_meta=meta, preamble=b"\0" * 128)
 
-    ds.file_meta.MediaStorageSOPInstanceUID=pydicom.uid.generate_uid()
-    ds.SOPInstanceUID = pydicom.uid.generate_uid()
+    ds.is_little_endian = True
+    ds.is_implicit_VR = False
 
-    sliceLocation = float(slicesLocation)
-    ds.sliceLocation = sliceLocation
+    ds.SOPClassUID = pydicom._storage_sopclass_uids.CTImageStorage
+    ds.PatientName = ''
+    ds.PatientID = "LIDC-IDRI-0004"
+
+    ds.StudyDate='20000101'
+    ds.SeriesDate='20000101'
+    ds.AcquisitionDate='20000101'
+    ds.ContentDate='20000101'
+    ds.OverlayDate='20000101'
+    ds.CurveDate='20000101'
+    ds.AcquisitionDateTime='20000101'
+    ds.StudyTime=''
+    ds.AcquisitioTime=''
+    ds.ContentTime=''
+    ds.AccessionNumber=''
+    ds.Manufacturer='GE MEDICAL SYSTEMS'
+    ds.ReferringPhysicianName =''
+    ds.ManufacturerModelName ='LightSpeed16'
+    ds.PatientBirthDate = ''
+    ds.PatientSex=''
+    ds.PatientAge=''
+    ds.LastMenstrualDate ='20000101'
+    ds.DeidentificationMethod  = 'DCM:113100/113105/113107/113108/113109/113111'
+    ds.PrivateCreator = 'CTP'
+    ds.Privatetagdata = 'LIDC-IDRI'
+    ds.Privatetagdata = '62796001'
+    ds.ContrastBolusAgent = 'OMNI'
+    ds.SoftwareVersions = '06MW03.5'
+    ds.DistanceSourcetoDetector = "949.075012"
+    ds.DistanceSourcetoPatient= "541.0"
+    ds.GantryDetectorTilt = "0.0"
+    ds.TableHeight= "174.699997"
+    ds.ExposureTime = "690"
+    ds.XRayTubeCurrent = "440"
+    ds.Exposure="14"
+    ds.GeneratorPower="52800"
+    ds.FocalSpot ="1.2"
+    ds.StudyID=''
+    ds.SeriesNumber="3000534"
+    ds.PositionReferenceIndicator ='SN'
+    ds.PixelPaddingValue = -2000
+    ds.ContentCreatorName =''
+    ds.StorageMediaFilesetUID=pydicom.uid.generate_uid()
+    ds.PersonName = 'Removed by CTP'
+    ds.UID=pydicom.uid.generate_uid()
+    ds.VerifyingObserverName =  'Removed by CTP'
+    ds.AdmittingDate   =     '20000101'
+    ds.ScheduledProcedureStepStartDateDA='20000101'
+    ds.ScheduledProcedureStepEndDateDA='20000101'
+    ds.PerformedProcedureStepStartDateDA='20000101'
+    ds.PlacerOrderNumberImagingServiLO= ''
+    ds.FilterOrderNumberImagingServiLO= ''
 
 
-    ds.PixelSpacing = r"0.822266\0.822266"
-    print(ds.sliceLocation)
-    a = float(-234.800003)
 
-    b = float(-170.500000)
 
-    ds.ImagePositionPatient = [a, b, sliceLocation]
-    
+
+
+
+    ds.Modality = "CT"
+    ds.SeriesInstanceUID = siuid
+    ds.SOPInstanceUID= pydicom.uid.generate_uid()
+    ds.StudyInstanceUID = pydicom.uid.generate_uid()
+    ds.FrameOfReferenceUID = pydicom.uid.generate_uid()
+    ds.ReferencedSOPInstanceUID = pydicom.uid.generate_uid()
+
+    # dt = datetime.datetime.now()
+    ds.ContentDate = dt.strftime('%Y%m%d')
+    timeStr = dt.strftime('%H%M%S.%f')  # long format with micro seconds
+    ds.ContentTime = timeStr
+
     ds.BitsStored = 16
     ds.BitsAllocated = 16
     ds.SamplesPerPixel = 1
     ds.HighBit = 15
-    ds.PixelPaddingValue = -2000
+
+    ds.ImagesInAcquisition = "1"
+
+    ds.Rows = image2d.shape[0]
+    ds.Columns = image2d.shape[1]
+    ds.InstanceNumber = i + 1
+
+    # sliceLocation=slice1Location-(sliceThickness*i)
+    # print(slicesLocation)
+    sliceLocation = float(slicesLocation)
+    ds.sliceLocation = sliceLocation
+    # print(ds.sliceLocation)
+
+    ds.PixelSpacing = r"0.822266\0.822266"
+    print(ds.sliceLocation)
+    a = float(-234.800003)
+    # a=a.replace(' ','').replace(',','.').replace("−", "-")
+    b = float(-170.500000)
+    # b=b.replace(' ','').replace(',','.').replace("−", "-")
+    print(a)
+    ds.ImagePositionPatient = [a, b, sliceLocation]
+    # ds.ImagePositionPatient = r"a\b\sliceLocation"
+
+    ds.ImageOrientationPatient = r"1\0\0\0\-1\0"
+    ds.ImageType = r"ORIGINAL\PRIMARY\AXIAL"
+
+    ds.RescaleIntercept = "-1024"
+    ds.RescaleSlope = "1"
+    ds.ScanOptions = 'HELICAL MODE'
+
+    ds.PhotometricInterpretation = "MONOCHROME2"
     ds.PixelRepresentation = 1
     ds.NumberofSlices = 150
+
+    ds.SpecificCharacterSet = 'ISO_IR 100'
     ds.KVP = 120
     ds.BodyPartExamined = 'CHEST'
-    ds.Modality = "MR"
+    ds.DataCollectionDiameter = "500"
+    ds.ReconstructionDiameter = "421"
+    ds.RotationDirection = 'CW'
+    ds.PatientPosition = 'FFS'
+    ds.ConvolutionKernel = 'STANDARD'
+    ds.LongitudinalTemporalInformation = 'MODIFIED'
+    ds.WindowCenter = '40'
+    ds.WindowWidth = '400'
 
+    pydicom.dataset.validate_file_meta(ds.file_meta, enforce_standard=True)
 
     print("Setting pixel data...")
     ds.PixelData = image2d.tobytes()
+    ds.save_as(filename_little_endian)
 
-    return ds
+    return
 
 slice1Location=-25
 sliceThickness=1.25
+dt = datetime.datetime.now()
+siuid=pydicom.uid.generate_uid()
 
 with torch.set_grad_enabled(False):
     for i, (image_app, label_app) in enumerate(
@@ -549,19 +660,21 @@ with torch.set_grad_enabled(False):
             t = out[0][j].cpu().numpy()
             t = cv2.resize(t, dsize=(512, 512), interpolation=cv2.INTER_CUBIC)
             slicesLocation = float(slice1Location - (sliceThickness * j))
-            ds = write_dicom(t, j, slicesLocation, dss)
-            ds.save_as(r"/content/drive/My Drive/NewDataset/new_saved_slices/%d.dcm" % (j + 1,))
+            filename_little_endian = ("/content/drive/My Drive/NewDataset/newSlices/%d.dcm" % (j + 1,))
+            ds = write_dicom(t, j, slicesLocation, filename_little_endian, dt, siuid)
+
 
         u = out1[0][0].cpu().numpy()
         u = cv2.resize(u, dsize=(512, 512), interpolation=cv2.INTER_CUBIC)
         slicesLocation = float(slice1Location - (sliceThickness * 0))
-        ds = write_dicom(t, 0, slicesLocation, dss)
-        ds.save_as(r"/content/drive/My Drive/NewDataset/new_saved_img/1.dcm")
+        filename_big_endian = ("/content/drive/My Drive/NewDataset/newImage/1.dcm")
+        ds = write_dicom(t, 0, slicesLocation, filename_big_endian, dt, siuid)
 
 
-dss=pydicom.read_file("/content/drive/My Drive/NewDataset/new_saved_img/1.dcm")
+
+dss=pydicom.read_file("/content/drive/My Drive/NewDataset/newImage/1.dcm")
 w=dss.pixel_array
-dss1=pydicom.read_file("/content/drive/My Drive/NewDataset/new_saved_slices/%d.dcm" % (slice_number,))
+dss1=pydicom.read_file("/content/drive/My Drive/NewDataset/newSlices/%d.dcm" % (slice_number,))
 v=dss1.pixel_array
 
 t=images_test1[image_number].reshape((256,256))
